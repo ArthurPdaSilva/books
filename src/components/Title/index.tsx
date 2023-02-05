3f1b2812bd1b9b8fef87a41820f4900dc3785b27
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-02-05 16:16:34 -0300

    fix: ajustes no tema dark

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,3 +1,4 @@
+import useTheme from "@/hooks/useTheme";
 import Link from "next/link";
 import React from "react";
 import styles from "./styles.module.scss";
@@ -9,14 +10,23 @@ interface TitleProps {
 }
 
 export default function Title({ header, linkText, link }: TitleProps) {
+  const { checked } = useTheme();
+
   return (
     <div
       className={
         linkText === "Voltar" ? `${styles.title} ${styles.back}` : styles.title
       }
     >
-      <h1>{header}</h1>
-      <Link href={link}>{linkText}</Link>
+      <h1 className={checked ? styles.darkTitle : styles.whiteTitle}>
+        {header}
+      </h1>
+      <Link
+        href={link}
+        className={checked ? styles.darkButton : styles.whiteButton}
+      >
+        {linkText}
+      </Link>
     </div>
   );
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,22 @@
 import Post from "@/components/Post";
 import Sidebar from "@/components/Sidebar";
+import Title from "@/components/Title";
+import useTheme from "@/hooks/useTheme";
 import Head from "next/head";
 import styles from "./styles.module.scss";
 
 export default function Dashboard() {
+  const { checked } = useTheme();
+
   return (
     <>
       <Head>
         <title>Books - Bem vindo a plataforma</title>
       </Head>
-      <div className={styles.container}>
+      <div className={checked ? styles.containerDark : styles.containerLight}>
         <Sidebar />
         <div className={styles.content}>
+          <Title header="Publicações" linkText="Publicar" link="/new" />
           <Post />
           <Post />
           <Post />
